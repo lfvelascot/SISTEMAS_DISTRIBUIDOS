a650d324c628a445944f1a1c77e777c01b29ff8b
Luis Felipe Velasco Tao <60698380+lfvelascot@users.noreply.github.com>
2021-10-11 13:00:51 -0500

    SQL CLOUD
    DOCUMENTO + BD

--- DATABASE/DDL.sql
+++ DATABASE/DDL.sql
@@ -1,6 +1,3 @@
--- -----------------------------------------------------
--- Schema bancapp_db
--- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `bancapp_db` DEFAULT CHARACTER SET utf8 ;
 USE `bancapp_db` ;
 
@@ -17,8 +14,7 @@ CREATE TABLE IF NOT EXISTS `bancapp_db`.`user` (
   PRIMARY KEY (`num_doc`))
 ENGINE = InnoDB;
 
-
--- -----------------------------------------------------
+--------------------------------------------------------
 -- Table `bancapp_db`.`rol`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `bancapp_db`.`rol` (
@@ -26,6 +22,9 @@ CREATE TABLE IF NOT EXISTS `bancapp_db`.`rol` (
   `descrip` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`nombre_rom`))
 ENGINE = InnoDB;
+
+INSERT INTO `bancapp_db`.`rol` (`nombre_rom`,`descrip`) VALUES ("ADMINISTRADOR","");
+INSERT INTO `bancapp_db`.`rol` (`nombre_rom`,`descrip`) VALUES ("USUARIO","");
 
 
 -- -----------------------------------------------------
@@ -37,6 +36,9 @@ CREATE TABLE IF NOT EXISTS `bancapp_db`.`estado_cuenta` (
   PRIMARY KEY (`nombre`))
 ENGINE = InnoDB;
 
+INSERT INTO `bancapp_db`.`estado_cuenta` (`nombre`,`descrip`) VALUES ("ACTIVA","");
+INSERT INTO `bancapp_db`.`estado_cuenta` (`nombre`,`descrip`) VALUES ("BLOQUEADA","");
+INSERT INTO `bancapp_db`.`estado_cuenta` (`nombre`,`descrip`) VALUES ("SUSPENDIDA","");
 
 -- -----------------------------------------------------
 -- Table `bancapp_db`.`tipo_cuenta`
@@ -47,6 +49,7 @@ CREATE TABLE IF NOT EXISTS `bancapp_db`.`tipo_cuenta` (
   PRIMARY KEY (`nombre`))
 ENGINE = InnoDB;
 
+INSERT INTO `bancapp_db`.`tipo_cuenta` (`nombre`,`descrip`) VALUES ("NORMAL","");
 
 -- -----------------------------------------------------
 -- Table `bancapp_db`.`cuenta_bancaria`
@@ -61,10 +64,10 @@ CREATE TABLE IF NOT EXISTS `bancapp_db`.`cuenta_bancaria` (
   `tipo_cuenta` VARCHAR(45) NOT NULL,
   `usuario` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`num`),
-  INDEX `fk_cuenta_bancaria_estado_cuenta1_idx` (`estado_cuenta` ASC) VISIBLE,
-  INDEX `fk_cuenta_bancaria_tipo_cuenta1_idx` (`tipo_cuenta` ASC) VISIBLE,
-  INDEX `fk_cuenta_bancaria_user1_idx` (`usuario` ASC) VISIBLE,
-  UNIQUE INDEX `usuario_UNIQUE` (`usuario` ASC) VISIBLE,
+  INDEX `fk_cuenta_bancaria_estado_cuenta1_idx` (`estado_cuenta` ASC)  ,
+  INDEX `fk_cuenta_bancaria_tipo_cuenta1_idx` (`tipo_cuenta` ASC)  ,
+  INDEX `fk_cuenta_bancaria_user1_idx` (`usuario` ASC)  ,
+  UNIQUE INDEX `usuario_UNIQUE` (`usuario` ASC)  ,
   CONSTRAINT `fk_cuenta_bancaria_estado_cuenta1`
     FOREIGN KEY (`estado_cuenta`)
     REFERENCES `bancapp_db`.`estado_cuenta` (`nombre`)
@@ -92,9 +95,9 @@ CREATE TABLE IF NOT EXISTS `bancapp_db`.`cuenta_app` (
   `ultimo_ingreso` DATETIME NOT NULL,
   `estado_cuenta_nombre` VARCHAR(45) NOT NULL,
   `rol_nombre_rom` VARCHAR(45) NOT NULL,
-  INDEX `fk_cuenta_app_estado_cuenta1_idx` (`estado_cuenta_nombre` ASC) VISIBLE,
-  INDEX `fk_cuenta_app_rol1_idx` (`rol_nombre_rom` ASC) VISIBLE,
-  INDEX `fk_cuenta_app_user1_idx` (`user_num_doc` ASC) VISIBLE,
+  INDEX `fk_cuenta_app_estado_cuenta1_idx` (`estado_cuenta_nombre` ASC)  ,
+  INDEX `fk_cuenta_app_rol1_idx` (`rol_nombre_rom` ASC)  ,
+  INDEX `fk_cuenta_app_user1_idx` (`user_num_doc` ASC)  ,
   PRIMARY KEY (`user_num_doc`),
   CONSTRAINT `fk_cuenta_app_estado_cuenta1`
     FOREIGN KEY (`estado_cuenta_nombre`)
@@ -124,8 +127,8 @@ CREATE TABLE IF NOT EXISTS `bancapp_db`.`transferencia` (
   `cuenta_destino` VARCHAR(100) NOT NULL,
   `fecha` DATETIME NOT NULL,
   PRIMARY KEY (`id_trans`),
-  INDEX `fk_transferencia_cuenta_bancaria_idx` (`cuenta_origen` ASC) VISIBLE,
-  INDEX `fk_transferencia_cuenta_bancaria1_idx` (`cuenta_destino` ASC) VISIBLE,
+  INDEX `fk_transferencia_cuenta_bancaria_idx` (`cuenta_origen` ASC)  ,
+  INDEX `fk_transferencia_cuenta_bancaria1_idx` (`cuenta_destino` ASC)  ,
   CONSTRAINT `fk_transferencia_cuenta_bancaria`
     FOREIGN KEY (`cuenta_origen`)
     REFERENCES `bancapp_db`.`cuenta_bancaria` (`num`)
@@ -143,12 +146,21 @@ ENGINE = InnoDB;
 -- Table `bancapp_db`.`accion`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `bancapp_db`.`accion` (
-  `nombre` INT NOT NULL,
+  `nombre` VARCHAR(45) NOT NULL,
   `descrip` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`nombre`))
 ENGINE = InnoDB;
 
-
+INSERT INTO `bancapp_db`.`accion`(`nombre`,`descrip`) VALUES("Login exitoso","");
+INSERT INTO `bancapp_db`.`accion`(`nombre`,`descrip`) VALUES("Login fallido","");
+INSERT INTO `bancapp_db`.`accion`(`nombre`,`descrip`) VALUES("Logout exitoso","");
+INSERT INTO `bancapp_db`.`accion`(`nombre`,`descrip`) VALUES("Logout fallido","");
+INSERT INTO `bancapp_db`.`accion`(`nombre`,`descrip`) VALUES("Ver estado Cuenta exitoso","");
+INSERT INTO `bancapp_db`.`accion`(`nombre`,`descrip`) VALUES("Ver estado Cuenta fallido","");
+INSERT INTO `bancapp_db`.`accion`(`nombre`,`descrip`) VALUES("Transferencia exitosa","");
+INSERT INTO `bancapp_db`.`accion`(`nombre`,`descrip`) VALUES("Transferencia fallida","");
+INSERT INTO `bancapp_db`.`accion`(`nombre`,`descrip`) VALUES("Ver historial transacciones exitoso","");
+INSERT INTO `bancapp_db`.`accion`(`nombre`,`descrip`) VALUES("Ver historial transacciones fallido","");
 -- -----------------------------------------------------
 -- Table `bancapp_db`.`log`
 -- -----------------------------------------------------
@@ -156,13 +168,13 @@ CREATE TABLE IF NOT EXISTS `bancapp_db`.`log` (
   `fecha` INT NOT NULL,
   `descrip` VARCHAR(45) NOT NULL,
   `cuenta` VARCHAR(100) NOT NULL,
-  `accion` INT NOT NULL,
+  `accion` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`fecha`),
-  INDEX `fk_log_cuenta_app1_idx` (`cuenta` ASC) VISIBLE,
-  INDEX `fk_log_accion1_idx` (`accion` ASC) VISIBLE,
+  INDEX `fk_log_cuenta_app1_idx` (`cuenta` ASC)  ,
+  INDEX `fk_log_accion1_idx` (`accion` ASC)  ,
   CONSTRAINT `fk_log_cuenta_app1`
     FOREIGN KEY (`cuenta`)
-    REFERENCES `bancapp_db`.`cuenta_app` (`contrasena`)
+    REFERENCES `bancapp_db`.`cuenta_app` (`user_num_doc`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_log_accion1`
@@ -177,10 +189,10 @@ ENGINE = InnoDB;
 -- Table `bancapp_db`.`rol_accion`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `bancapp_db`.`rol_accion` (
-  `accion` INT NOT NULL,
+  `accion` VARCHAR(45) NOT NULL,
   `rol` VARCHAR(45) NOT NULL,
-  INDEX `fk_rol_accion_accion1_idx` (`accion` ASC) VISIBLE,
-  INDEX `fk_rol_accion_rol1_idx` (`rol` ASC) VISIBLE,
+  INDEX `fk_rol_accion_accion1_idx` (`accion` ASC)  ,
+  INDEX `fk_rol_accion_rol1_idx` (`rol` ASC)  ,
   CONSTRAINT `fk_rol_accion_accion1`
     FOREIGN KEY (`accion`)
     REFERENCES `bancapp_db`.`accion` (`nombre`)
